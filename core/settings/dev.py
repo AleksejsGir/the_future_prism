b29257d862fc4d8c33348263c76609afd8c6267b
AleksejsGir <giruckisaleksejs@gmail.com>
2025-02-13 10:00:33 +0200

    Update dev.py

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -10,8 +10,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DATABASE_NAME', 'future_prism'),
-        'USER': os.getenv('DATABASE_USER', 'future_prism_user'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'linda1990'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST', 'localhost'),
         'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
