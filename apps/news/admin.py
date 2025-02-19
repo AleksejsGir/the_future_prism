4b9c7141e291071b50f7ae4d886ab66611543e35
AleksejsGir <giruckisaleksejs@gmail.com>
2025-02-19 22:36:08 +0000

    UPDATE POINT Оптимизация CSS: уст 19.02.2025ранены дубликаты стилей

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -4,35 +4,38 @@ from .models import Category, News
 from tinymce.widgets import TinyMCE
 from django.db import models
 
+admin.site.site_header = "The Future Prism Admin"
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+    list_display = ('id', 'name', 'description') # ✅ Добавил отображение описания
     search_fields = ('name',)
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'published_date', 'view_count', 'category', 'image_preview')
     list_filter = ('published_date', 'category')
-    search_fields = ('title', 'content')
-    readonly_fields = ('image_preview',)
-    prepopulated_fields = {'slug': ('title',)}
-    ordering = ['-published_date']
+    search_fields = ('title', 'content') # ✅ Добавил поиск по содержимому
+    readonly_fields = ('image_preview',) # ✅ Добавил поле только для чтения
+    prepopulated_fields = {'slug': ('title',)}  # ✅ Автозаполнение slug
+    ordering = ['-published_date']  # ✅ Сортировка по умолчанию
     fieldsets = (
         ('Основная информация', {
-            'fields': ('title', 'slug', 'content', 'image', 'category', 'published_date')
+            'fields': ('title', 'slug', 'content', 'image', 'category', 'published_date') # ✅ Добавил slug
         }),
         ('Дополнительные данные', {
-            'fields': ('view_count', 'image_preview'),
+            'fields': ('view_count', 'image_preview'), # ✅ Добавил image_preview
             'classes': ('collapse',)
         }),
     )
 
     formfield_overrides = {
-        models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 15})},  # ✅ Добавили TinyMCE
+        models.TextField: {'widget': TinyMCE(attrs={'cols': 80, 'rows': 15})},  # ✅ Добавил TinyMCE
     }
 
     def image_preview(self, obj):
         if obj.image:
             return format_html('<img src="{}" style="max-width: 150px; max-height:150px;" />', obj.image.url)
-        return '-'
+        return '-' # ✅ Добавил отображение изображения
     image_preview.short_description = 'Изображение'
